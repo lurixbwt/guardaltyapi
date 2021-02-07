227f34f01688f1606e2c69102ef594af0d911d1f
lurixbtw <lurixst@gmail.com>
2021-02-07 15:49:41 +0300

    reqwerwq
    rwqer

---
+++ git.bat
@@ -0,0 +1,7 @@
+echo "# guardaltyapi" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/lurixbwt/guardaltyapi.git
+git push -u origin12 main
